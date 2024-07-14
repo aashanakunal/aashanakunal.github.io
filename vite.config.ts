8ae658d826350460a04344627d058f40fb0e5d10
Kunal <kunal.m.malkan@gmail.com>
2024-07-14 13:54:16 -0400

    Updated dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/",
+  base: "/aashanakunal.github.io",
   plugins: [react()],
   resolve: {
     alias: {
