c1dc7831bcf3a004f60bf0f29f2c47d045929115
Kunal <kunal.m.malkan@gmail.com>
2024-07-14 11:52:45 -0400

    Created home page

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import aashanakunal from "@/assets/aashana-kunal.svg";
+import ak from "@/assets/ak-logo.svg";
+
+const AlbumUrl = "https://photos.app.goo.gl/dEsn2jYtoUxdpLgM7";
+
+function App() {
+  return (
+    <div className="flex min-h-screen min-w-full flex-col items-center justify-center bg-cover pb-24">
+      <div className="absolute top-0 flex-col items-center justify-center pt-8">
+        <img src={ak} className="m-auto max-w-12 p-3" />
+        <div className="font-playfair font-semibold tracking-wider">
+          12 . 09 . 23
+        </div>
+      </div>
+      <img src={aashanakunal} className="h-auto max-w-lg" />
+      <div className="flex max-w-sm rounded-xl bg-gradient-to-tr from-yellow-800 to-blue-300 p-0.5 shadow-lg">
+        <Button
+          className="isolate bg-primary/50 font-thin tracking-widest shadow-lg ring-1 ring-black/5"
+          onClick={() => {
+            window.open(AlbumUrl, "_blank");
+          }}
+        >
+          View Album
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
